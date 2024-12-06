3afac106742e8e3910d22d3e9dc8d1a11a769f82
Riley-Cox <rileyc39@gmail.com>
2024-12-05 16:21:27 -0800

    Changes including the UART transmission output and buzzer output

--- software/src/ThunderBuddy.c
+++ software/src/ThunderBuddy.c
@@ -16,7 +16,7 @@
 #define LED_DELAY_MS 250
 
 //Debug macro expansions
-#ifdef FLASHPRINT
+#ifdef PRINTFLASH
 #define FLASH
 #define PRINT
 #endif
@@ -36,21 +36,21 @@
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS 17
-#define PIN_SCK 18
-#define PIN_MOSI 19
+//#define PIN_MISO 4 
+#define PIN_CS 5 
+#define PIN_SCK 2 
+#define PIN_MOSI 3 
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+//#define I2C_PORT i2c0
+//#define I2C_SDA 8
+//#define I2C_SCL 9
 
 // ADC defines
-#define THRESHOLD 1
-#define ADC_PIN 26
+#define THRESHOLD 0.5
+#define ADC_PIN 28
 //#define ADC_IRQ 26
 #define REFERENCE_VOLTAGE 3.3
 
@@ -64,14 +64,19 @@
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 
+
+#define GPIO14 14
+
+#define BUZZER 0
+
 //ADF7020 defines (Values to be determined)
 
 #define R0 UINT32_C(0x1138DCA0)
 #define R1 UINT32_C(0x00021011)
-#define R2 UINT32_C(0x00003ED2)
+#define R2 UINT32_C(0x20007ED2)
 
 //7020 transmission defines
-#define TXRXDATA_PIN 12
+#define TXRXDATA_PIN 9
 #define TX_T         200
 #define PREAMBLE     0xAA
 #define DEVICE_ID    0x96
@@ -85,32 +90,43 @@ void writeRegister(uint32_t data); //Write to device over spi
 void transceiverInit();
 void transmit_id();
 void txPacket(char packet);
+void strobeSPI();
 
 int main()
 {
     // IO initialization
     stdio_init_all();
     sleep_ms(5000);
-    gpio_set_dir(TXRXDATA_PIN, true);
+    gpio_init(TXRXDATA_PIN);
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER,GPIO_OUT);
+    gpio_set_dir(TXRXDATA_PIN, GPIO_OUT);
     adc_init();
     adc_gpio_init(ADC_PIN);
-    adc_select_input(0);
-
+    adc_select_input(2);
+    //gpio_init(PIN_MISO);
+    //gpio_init(PIN_SCK);
+    //gpio_init(PIN_MOSI);
+    uart_init(uart1,9600);
+    gpio_set_function(UART_TX_PIN,UART_FUNCSEL_NUM(uart1,UART_TX_PIN));
+    
   // variables for ADC conversion
-    float conversion = (REFERENCE_VOLTAGE) / (1 << 12);
-    uint16_t rfInput;
+    const float conversion = (REFERENCE_VOLTAGE) / (1 << 12);
+    uint16_t rawInput;
+    float rfInput;
 
+   // gpio_put(GPIO14,1);
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000 * 1000);
+    /*spi_init(SPI_PORT, 1000 * 1000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_init(PIN_CS);
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    spi_set_format(SPI_PORT,16,0,0,SPI_MSB_FIRST);
 
-    // Chip select is active-low, so we'll initialise it to a driven-high state
     gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    
+    gpio_put(PIN_CS, 0);
+   */ 
     // Set up WiFi LED 
     if(cyw43_arch_init()){
     printf("WiFi LED initialization failed\n");
@@ -121,22 +137,31 @@ int main()
     #ifdef PRINT
     printf("Initializing transceiver\n");
     #endif
-    transceiverInit();
+    //transceiverInit();
+  
 
     while (true)
     {
 	#ifndef FORCEINT
-        rfInput = adc_read() * conversion;
+        rawInput = adc_read();
+        rfInput = rawInput * conversion;
 	#else
 	rfInput = 10;
 	#endif
+  #ifdef PRINT
+      printf("The raw input is %d, rfInput is %f\n",rawInput, rfInput);
+  #endif
         if (rfInput > THRESHOLD){
 	#ifdef PRINT
 	printf("We are checking if the input exceeds the threshold\n");
 	#endif
+            gpio_put(BUZZER,1);
             overThreshold();
 	}
     }
+    #ifdef PRINT
+    printf("We have left the while loop for some reason");
+    #endif
 }
 
 //function definitions
@@ -148,10 +173,12 @@ void overThreshold(){ // we have gone over our threshold this is where our trans
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,true); //Set LED to be on
     #endif
     transmit_id();
+    sleep_ms(1000);
     #ifdef FLASH
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,false);
     sleep_ms(250);
     #endif
+  return;
 }
 
 void writeRegister(uint32_t data){ // Sending data over SPI
@@ -159,15 +186,24 @@ void writeRegister(uint32_t data){ // Sending data over SPI
     printf("We are sending %x over SPI\n", data);
     #endif
     uint16_t buffer[2]; //When we send the data over SPI its in one word so we just need to split the word into two halfwords
-    buffer[0] = data & 0xFFFF; // Lower bits 
-    buffer[1] = (data >> 16) & 0xFFFF; // Upper bits
-    spi_write16_blocking(spi0,buffer,2);
+    buffer[1] = data & 0xFFFF; // Lower bits 
+    buffer[0] = (data >> 16) & 0xFFFF; // Upper bits
+    spi_write16_blocking(SPI_PORT,buffer,2);
+    
+    strobeSPI(); 
+}
+
+void strobeSPI(){
+  gpio_put(PIN_CS,1);
+  sleep_ms(1);
+  gpio_put(PIN_CS,0);
 }
 
 void transceiverInit(){ // Initializing the AD7020 over SPI
     #ifdef PRINT
     printf("We are initializing the 7020 transceiver\n");
     #endif
+    writeRegister(R0);
     writeRegister(R0);
     writeRegister(R1);
     writeRegister(R2);
@@ -181,13 +217,18 @@ void transmit_id(){
     char id = DEVICE_ID;
     char callsign[CALL_WIDTH] = CALLSIGN;
     char preamble = PREAMBLE;
-    
-    // Send message components
-    txPacket(preamble);
-    for (int i = 0; i < CALL_WIDTH; i++) txPacket(callsign[i]);
-    txPacket(id); 
 
-    gpio_put(TX_T, 0); // Set pin low to mute PA
+    uart_puts(uart1,CALLSIGN);
+    // Send message components
+    gpio_put(TXRXDATA_PIN,1);
+    sleep_ms(250);
+    gpio_put(TXRXDATA_PIN,0);
+//    txPacket(preamble);
+  //  for (int i = 0; i < CALL_WIDTH; i++) txPacket(callsign[i]);
+ //   txPacket(id); 
+
+    gpio_put(TXRXDATA_PIN, 0); // Set pin low to mute PA
+  return;
 }
     
 
@@ -201,5 +242,6 @@ void txPacket(char packet){
         sleep_ms(TX_T/2);
         gpio_put(TXRXDATA_PIN, ( (packet >> i) & 1));
         sleep_ms(TX_T/2);
+    return;
 	}
 }
